a89305b4c34a5184aa2154180b89208f4eca0b1d
gyu-min <lee940910@naver.com>
2018-09-17 18:41:30 +0900

    drill05_함수 이름 변경
    함수 이름을 실수로 작성하여 다시 변경

--- Drill 05/drill05.py
+++ Drill 05/drill05.py
@@ -6,49 +6,61 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-def move_203_to_535():
+def move_203_535():
+    x, y = 800 // 2, 90
+    frame = 0
+
+    while (x > 203 and y < 535):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x -= 5
+        y += 5
+        delay(0.05)
+        get_events()
+
+def move_132_243():
     pass
 
-def move_132_to_243():
+def move_535_470():
     pass
 
-def move_535_t0_470():
+def move_477_203():
     pass
 
-def move_477_to_203():
+def move_715_136():
     pass
 
-def move_715_to_136():
+def move_316_225():
     pass
 
-def move_316_to_225():
+def move_510_92():
     pass
 
-def move_510_to_92():
+def move_692_518():
     pass
 
-def move_692_to_518():
+def move_682_336():
     pass
 
-def move_682_to_336():
-    pass
-
-def move_712_to_349():
+def move_712_349():
     pass
 
 
 
 
 while True:
-    move_203_to_535()
-    move_132_to_243()
-    move_535_t0_470()
-    move_477_to_203()
-    move_715_to_136()
-    move_316_to_225()
-    move_510_to_92()
-    move_692_to_518()
-    move_682_to_336()
-    move_712_to_349()
+    move_203_535()
+    move_132_243()
+    move_535_470()
+    move_477_203()
+    move_715_136()
+    move_316_225()
+    move_510_92()
+    move_692_518()
+    move_682_336()
+    move_712_349()
 
 
