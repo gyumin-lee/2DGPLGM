0df1451b78549bf1414e2fade87c2fb978cea6d9
gyu-min <lee940910@naver.com>
2018-10-04 17:13:38 +0900

    drill08-2 캐릭터가 랜덤값에 따라 움직이게 하기

--- Drill 08/dril 08-2.py
+++ Drill 08/dril 08-2.py
@@ -13,20 +13,31 @@ def move_randompoint(p1, p2):
 
     frame =0
     moving = 100
-    x = 25
-    y = 50
+    x = 0
+    y = 0
 
-    for i in range(0,50+1, 2):
+    for i in range(0, 50+1, 2):
         t = i/100
         x = (2 * t ** 2 - 3 * t + 1) * p1[0] + (-4 * t ** 2 + 4 * t) * p2[0]
         y = (2 * t ** 2 - 3 * t + 1) * p1[1] + (-4 * t ** 2 + 4 * t) * p2[1]
         clear_canvas()
 
-        
+
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        character.clip_draw(frame * 100, moving, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 7
+        delay(0.05)
+
+i = 0
+
+points = [(random.randint(0 + 50, KPU_WIDTH - 50), random.randint(0 + 50, KPU_HEIGHT - 50)) for i in range(10)]
+
+while True:
+    move_randompoint(points[i - 1], points[i])
 
 
+    i = (i + 1) % 10
 
-kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-update_canvas()
 
 close_canvas()
