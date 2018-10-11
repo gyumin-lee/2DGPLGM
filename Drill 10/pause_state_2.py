658c2d89c6d5c0ce7da515f8181fd1ecdf84cff9
gyu-min <lee940910@naver.com>
2018-10-11 23:10:09 +0900

    dil10 커밋 5
    그림판으로 자른 이미지를 보여주는 클라스 생성

--- Drill 10/pause_state_2.py
+++ Drill 10/pause_state_2.py
@@ -7,5 +7,24 @@ name = "PauseState_2"
 image = None
 
 
+class Pause:
+    def __init__(self):
+        self.image = load_image('pause_2.png')
+        self.switch = 0
 
-c
+    def draw(self):
+        if self.switch == 1:
+            self.image.draw(400, 300)
+
+    def update(self):
+        self.switch = (self.switch + 1) % 5
+        delay(0.05)
+
+def enter():
+    global image
+    image = Pause()
+
+def exit():
+    global image
+    del(image)
+
